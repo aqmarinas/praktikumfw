0ea6d374c4dda497361fc1b50d4edaeab1bb0cb0
Arina <aqmarinasha@gmail.com>
2024-10-07 08:53:09 +0700

    bab 4

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('title', 'Home Page')
+
+@section('content')
+    <h1>Welcome Home Page</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,7 +10,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified', 'RoleCheck:admin'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -18,3 +19,10 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::resource('products', ProductController::class);
+
+// temporary
+Route::get('/home', function () {
+    return view('home');
+});
