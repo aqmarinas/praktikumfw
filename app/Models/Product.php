e1013deeca0dd6c65e36caa25456ea556a4a93cf
Arina <aqmarinasha@gmail.com>
2024-11-16 20:19:21 +0700

    feat: add FK

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,4 +10,9 @@ class Product extends Model
     use HasFactory;
 
     protected $fillable = ['product_name', 'unit', 'type', 'information', 'qty', 'producer'];
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
 }
